6effae69f75865629c1578fb89573d582e730d70
Tim <tim@tamimimail.com>
2020-05-28 15:11:49 +0100

    Added start script and built app

--- client/build/service-worker.js
+++ client/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5bd78fa88b0cd0edcb5435d70c74ea07.js"
+  "/precache-manifest.933d3cffa25f5e92b6528d810894172f.js"
 );
 
 self.addEventListener('message', (event) => {
